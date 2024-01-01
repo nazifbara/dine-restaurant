1f8480ed2b7803412fd37870e8570b8cd74ac8ae
nazifbara <nazifbarassounon@gmail.com>
2024-01-01 16:35:21 +0100

    add typography classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1 class="text-5xl">Dine</h1>
+<h2 class="text-5xl mb-5">Typography</h2>
+
+<div class="grid gap-2">
+	<p class="typo typo--xl">Jived Fox</p>
+	<p class="typo typo--l">Jived fox nymph grabs quick waltz</p>
+	<p class="typo typo--m">Jived fox nymph grabs quick waltz</p>
+	<p class="typo typo--s">family gathering</p>
+	<p class="typo typo--body-1">
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
+		aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium,
+		ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu
+		nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+	</p>
+	<p class="typo typo--body-2">
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
+		aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium,
+		ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu
+		nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+	</p>
+</div>
