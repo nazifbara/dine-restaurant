2055828149b26c6e4f4bdf677c03e38f8cf63525
nazifbara <nazifbarassounon@gmail.com>
2024-01-03 18:24:28 +0100

    implement the home page header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,51 @@
-<h2 class="text-5xl mb-5">Button</h2>
+<header
+	class={`
+  dark
+  grid
+  items-center
+  justify-center
+  h-[748px]
+  bg-no-repeat
+  bg-top
+  bg-cover
+  bg-[url('/assets/homepage/hero-bg-mobile@2x.jpg')]
+  md:bg-[url('/assets/homepage/hero-bg-tablet@2x.jpg')]
+  md:h-[992px]
+  lg:bg-[url('/assets/homepage/hero-bg-desktop@2x.jpg')]
+  lg:h-[820px]
+  lg:justify-start
+  lg:items-start
+`}
+>
+	<div
+		class={`
+      bg-primary-2/60
+      rounded-lg
+      grid
+      py-4
+      px-6
+      justify-items-center
+      content-center
+      text-center
+      max-w-[507px]
+      md:px-14
+      lg:justify-items-start
+      lg:px-0
+      lg:text-left
+      lg:ml-[165px]
+      lg:bg-transparent
+      lg:mt-16
+    `}
+	>
+		<img src="/assets/logo.svg" alt="Site logo" class="mb-9 lg:mb-[153px]" />
 
-<div class="grid gap-2">
-	<div class="p-5">
-		<button class="btn">button</button>
+		<h1 class="typo typo--xl mb-5">Exquisite dining since 1989</h1>
+
+		<p class="mb-12">
+			Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from
+			the comfort of our farmhouse.
+		</p>
+
+		<button class="btn">book a table</button>
 	</div>
-	<div class="dark bg-black p-5">
-		<button class="btn"> button </button>
-	</div>
-</div>
+</header>
